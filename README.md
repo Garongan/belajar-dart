c3b3f452b1a4292486f37a60b1e3b9e238ec454b
Alvindo Tri Jatmiko <alvindo56@gmail.com>
2024-09-21 18:29:11 +0700

    feat: learn collections

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-Dart Control Flow 
-- if else condition
-- for loops
-- while and do while loops
-- break and continue
-- switch and case
+Dart Collections 
+- List
+- Set
+- Map
--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -1,153 +1,89 @@
 import 'dart:io';
 
-import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
-
 void main(List<String> arguments) {
-  // conditional control flow
-  var isRaining = true;
-  print('prepare to go office');
-  if (isRaining) {
-    print("Oh it's raining bring umbrella");
-  } else {
-    print('walk');
+  // Collections : List, Set, Map
+  // List
+  // explicits declarations
+  List<int> numberList = [1, 2, 3, 4, 5];
+  // implicits declarations
+  var stringList = ["one", "two", "three"];
+  // dynamic list
+  List mixTypeList = [1, "two", 3];
+  print(mixTypeList.runtimeType);
+  print(mixTypeList[1]); // access value in list by index
+  // print(mixTypeList[3]); // this line will get error index out of bound
+  // access all value in list with for loop
+  for (int i = 0; i < mixTypeList.length; i++){
+    print(mixTypeList[i]);
   }
-
-  var openHours = 7;
-  var closeHours = 20;
-  var now = 8;
-  if (now > openHours && now < closeHours) {
-    print('We are open');
-  } else {
-    print('We are close');
+  // access all value in list with foreach loop type 1
+  mixTypeList.forEach((value) => print(value)); // using lambda expression
+  // access all value in list with foreach loop type 2
+  for (var value in mixTypeList){
+    print(value);
   }
+  // adding value to list
+  stringList.add("four");
+  print(stringList);
+  // insert value with specific index in list
+  mixTypeList.insert(0, 'zero');
+  print(mixTypeList);
+  // change the value of list by index
+  mixTypeList[1] = 'one';
+  print(mixTypeList);
+  // remove value in list
+  numberList.remove(1); // remove by value
+  numberList.removeAt(2); // remove by index
+  numberList.removeLast(); // remove last index in list
+  numberList.removeRange(0, 2);
+  print(numberList);
 
-  var score = 85;
+  // spread operator in list
+  var newMixListType = [stringList, mixTypeList];
+  print(newMixListType);
+  var newMixTypeListWithSpreadOperator = [...stringList, ...mixTypeList]; // spread operator is performing copy all value in list
+  print(newMixTypeListWithSpreadOperator);
 
-  print('Nilai Anda: ${calculateScore(score)}');
+  // add list with spread operator and null aware
+  List<dynamic>? nullList;
+  List<dynamic>? list2 = [0, ...?nullList];
+  print(list2);
 
-  var shopStatus =
-      now > openHours ? "Hello, we're open" : "Sorry, we've closed";
+  // collection set is collection that receive unique value or not duplicated value
+  Set<int> numberSet = new Set.from([1, 2, 3, 4, 5]); // the old way
+  Set<int> newNumberSet = {1, 2, 3, 4, 5}; // using set literal
+  print(numberSet);
+  print(newNumberSet);
+  // adding data to set collection
+  numberSet.add(6); // and one data to set collection
+  newNumberSet.addAll({6, 7, 8}); // add all new set collection
+  print(numberSet);
+  print(newNumberSet);
+  // remove data in set by value
+  newNumberSet.remove(1);
+  // get data in set collection by value
+  print(numberSet.elementAt(3));
+  var union = numberSet.union(newNumberSet);
+  print('union $union');
+  var intersection = numberSet.intersection(newNumberSet);
+  print('intersection $intersection');
 
-  var name;
-  var buyer = name ?? 'user';
-  print(buyer);
+  // Map collection is collection that type with key and value
+  var capital = {
+    "Jakarta": "Indonesia",
+    "London": "England",
+    "Canberra": "Australia"
+  };
+  
+  print(capital['London']);
 
-  // for loops
-  for (int i = 1; i < 10; i++) {
-    print(i);
-  }
+  var mapKeys = capital.keys;
+  print(mapKeys);
 
-  // star pyramid
-  for (int i = 1; i <= 10; i++) {
-    for (int j = 1; j <= i; j++) {
-      stdout.write("*");
-    }
-    print("");
-  }
+  var mapValues = capital.values;
+  print(mapValues);
 
-  // while loop and do while loop
-  var i = 1;
-  while (i <= 10) {
-    print('$i in while loop');
-    i++;
-  }
-
-  // do while loop
-  do {
-    print('$i in do while loop');
-    i++;
-  } while (i <= 20);
-
-  // challenge reverse pyramid
-  var j = 10;
-  var k = 1;
-  while (j >= 1) {
-    k = 1;
-    while (k <= j) {
-      stdout.write("*");
-      k++;
-    }
-    print("");
-    j--;
-  }
-
-  // break and continue in looping
-
-  // break is for stop the looping when condition meet
-  // bilangan prima di bawah 100
-  var primeNumbers = [
-    2,
-    3,
-    5,
-    7,
-    11,
-    13,
-    17,
-    19,
-    23,
-    29,
-    31,
-    37,
-    41,
-    43,
-    47,
-    53,
-    59,
-    61,
-    67,
-    73,
-    79,
-    83,
-    89,
-    97
-  ];
-
-  var searchNumber = 13;
-  print('Bilangan prima di antara 1-100: $searchNumber');
-
-  for (int i = 0; i < primeNumbers.length; i++) {
-    if (searchNumber == primeNumbers[i]) {
-      print('$searchNumber adalah bilangan prima ke-${i + 1}');
-      break;
-    }
-    print('$searchNumber != ${primeNumbers[i]}');
-  }
-
-  // continue is for skip loop iteration when condition meet
-  for (int i = 1; i <= 10; i++) {
-    if (i % 3 == 0) {
-      continue;
-    }
-    print(i);
-  }
-
-  // switch case
-  final number = 1;
-  final number2 = 2;
-  final operator = "+";
-
-  switch (operator) {
-    case '+':
-      print('$number + $number2 = ${number + number2}');
-      break;
-    case '-':
-      print('$number - $number2 = ${number - number2}');
-      break;
-    default:
-      print("operator not found");
-  }
-}
-
-String calculateScore(num score) {
-  if (score > 90) {
-    return 'A';
-  } else if (score > 80) {
-    return 'B';
-  } else if (score > 70) {
-    return 'C';
-  } else if (score > 60) {
-    return 'D';
-  } else {
-    return 'E';
-  }
+  // adding new data to map
+  capital['New Delhi'] = 'India';
+  print(capital);
 }
