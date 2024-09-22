8473e2753e0773ab9ec4380870bdbb8c0b6a7199
alvindo jatmiko <alvindo56@gmail.com>
2024-09-22 13:51:59 +0700

    feat: learn about cascade notation in class

--- README.md
+++ README.md
@@ -12,6 +12,6 @@ Object Oriented Programming
   - Polymorphism dalam bahasa Yunani berarti “banyak bentuk.” Sederhananya objek dapat memiliki bentuk atau implementasi yang berbeda-beda pada satu metode yang sama. Semua hewan bernafas, namun tentu kucing dan ikan memiliki cara bernafas yang berbeda. Perbedaan bentuk atau cara pernafasan tersebut merupakan contoh dari polymorphism.
 
 Learn about:
-- class
+- classw
 - property and methods
 - constructor
--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -1,4 +1,5 @@
 import 'animal.dart';
+import 'address_book.dart';
 
 void main(List<String> arguments) {
   // to create new object from class
@@ -9,7 +10,16 @@ void main(List<String> arguments) {
   sheep.sleep();
   print(sheep.weight);
 
-  var cat = Animal.cat("Coki", 18.5);
+  var cat = Animal.cat('', 18.5)
+  ..name = 'Coki' // performing cascade operator to call property
+  ..eat(); // performing cascade operator to call method
   cat.sleep();
   print(cat.age);
+
+  final addressBook = (AddressBookBuilder()
+    ..name = 'jenny'
+    ..email = 'jenny@example.com'
+    ..phone = '415-555-0100')
+      .build();
+  print(addressBook.name);
 }
